d691b39dea210ebf9f3101872103105fbc462b3e
mikel335 <zmichael852@gmail.com>
2024-09-09 14:25:35 -0400

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.GetFizzBuzz(0);
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        fizzBuzz.main(args);
+    }
+}
