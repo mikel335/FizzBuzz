5f64678bfa8e87e5dffd559b9079f701becf017d
mikel335 <zmichael852@gmail.com>
2024-09-09 14:50:16 -0400

    committed

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,12 @@
 public class Multiples {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 1000;) {
+            if (i%3 == 0 || i%5 == 0) {
+                count++;
+            }
+            i++;
+        }
+        System.out.println(count);
+    }
 }
